ef213a8d9f56c09b7eea4ee635a6a1fe681c8f38
Carlos Jouanne <cjjouanne@uc.cl>
2021-10-06 19:37:42 -0300

    second try

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96fd27686f141779c3e1ff0fef57770e.js"
+  "/backend-zmartboard/precache-manifest.e7107b48a007e4044cf5a238752fd1eb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/backend-zmartboard/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
